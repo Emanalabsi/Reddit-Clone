ecdebf771afad519bc97a349739e68de016e8c37
Emanalabsi <emanabsi30@gmail.com>
2023-04-21 23:53:33 +0300

    add count votes query

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,8 +1,20 @@
-const { getِUserPostsQuery } = require("../database/queries/posts");
+const getِUserPostsQuery = require("../database/queries/profile");
 
 const getUserPosts = (req, res, next) => {
   const { id } = req.user;
-  getِUserPostsQuery().then();
+  getِUserPostsQuery(id)
+    .then((result) => {
+      if (result.rowCount === 0) {
+        throw new CustomError("No posts found", 401);
+      }
+    })
+    .then((data) =>
+      res.json({
+        error: false,
+        data: data.rows,
+      })
+    )
+    .catch((err) => next(err));
 };
 
 module.exports = getUserPosts;
