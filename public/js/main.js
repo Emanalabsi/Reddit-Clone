e299ab3b500e88a6ac1bea4f7ec7a3659d531d9d
Emanalabsi <emanabsi30@gmail.com>
2023-04-23 03:04:12 +0300

    handle signup login forms

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,48 @@
-const signupBtn = document.querySelector(".signup");
+const signupButton = document.querySelector("#signup-button");
+const loginButton = document.querySelector("#login-button");
 
-signupBtn.addEventListener("click", fetch("/signup").then(console.log));
+const loginModal = document.querySelector("#login-modal");
+const signupModal = document.querySelector("#signup-modal");
+
+const signupForm = document.querySelector("#signup");
+const loginForm = document.querySelector("#login");
+
+const closeModalButtons = document.getElementsByClassName("close");
+
+signupButton.addEventListener("click", () => {
+  document.getElementById("signup-modal").style.display = "block";
+});
+
+loginButton.addEventListener("click", () => {
+  document.getElementById("login-modal").style.display = "block";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === loginModal) {
+    loginModal.style.display = "none";
+  } else if (event.target === signupModal) {
+    signupModal.style.display = "none";
+  }
+});
+
+for (const button of closeModalButtons) {
+  button.addEventListener("click", (event) => {
+    const target = event.target.parentNode.parentNode.parentNode;
+    if (target === loginModal) {
+      loginModal.style.display = "none";
+    } else if (target === signupModal) {
+      signupModal.style.display = "none";
+    }
+  });
+}
+
+const postRequest = (url, data) => {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify(data),
+  }).then((result) => result.json());
+};
---
+++ public/js/users.js
@@ -0,0 +1,22 @@
+const getFormInputs = (form) => {
+  const inputs = form.querySelectorAll("input");
+  const data = {};
+  inputs.forEach((element) => {
+    data[element.name] = element.value;
+  });
+  return data;
+};
+
+const signup = (userData) =>
+  postRequest("/api/v1/users/signup", { ...userData });
+
+const login = (userData) => {
+  postRequest("/api/v1/users/login", { ...userData });
+};
+
+signupForm.addEventListener("click", (event) => {
+  signup(getFormInputs(event.target));
+});
+loginForm.addEventListener("click", (event) => {
+  login(getFormInputs(event.target));
+});
